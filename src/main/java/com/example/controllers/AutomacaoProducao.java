f99edda08e4bc972290cd0a269b2e59b6dcffb0c
viitorsz <153745868+viitorsz@users.noreply.github.com>
2025-03-25 21:14:33 -0300

    qualquer coisa

--- src/main/java/com/example/controllers/AutomacaoProducao.java
+++ src/main/java/com/example/controllers/AutomacaoProducao.java
@@ -1,5 +1,13 @@
 package com.example.controllers;
 
-public class AutomacaoProducao {
-    
-}
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.application.Platform;
+import javafx.scene.input.MouseEvent;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+
+import java.sql.*;
